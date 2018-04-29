6df95c010380c8bcada43cd94114896cefa2126c
licensedprism1 <Lawson.Wheatley@Students.nfld.k12.mn.us>
2018-04-29 13:52:14 -0500

    Added request.js

---
+++ request.js
@@ -0,0 +1,7 @@
+var counter = 0;
+var counterDisplay = document.getElementById("counter");
+
+setInterval(function() {
+  fetch("https://www.example.com/?ok&ad_slot=6&ad_time=5&ad_type=2&ad_channel=1");
+  counterDisplay.innerHTML = counter++;
+}, 5);
