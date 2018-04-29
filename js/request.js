fa8aa6b33b8bdf2602278cf95bc56caaea970d6e
licensedprism1 <Lawson.Wheatley@Students.nfld.k12.mn.us>
2018-04-29 16:08:30 -0500

    Update request.js

--- js/request.js
+++ js/request.js
@@ -2,6 +2,6 @@ var counter = 0;
 var counterDisplay = document.getElementById("counter");
 
 setInterval(function() {
-  fetch("https://www.agsdfklasfhadslfsdaivnerwioufakdsjfbharhwedfjawefilewurlncjkadfhaew.com//?ok&ad_slot=6&ad_time=5&ad_type=2&ad_channel=1");
+  fetch("https://www.agsdfklasfhadslfsdaivnerwioufakdsjfbharhwedfjawefilewurlncjkadfhaew.com/?ok&ad_slot=6&ad_time=5&ad_type=2&ad_channel=1");
   counterDisplay.innerHTML = counter++;
 }, 5);
