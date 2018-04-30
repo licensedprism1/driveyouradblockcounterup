d3bff7f448d5c7a5e8d414193c2730b76d810356
licensedprism1 <Lawson.Wheatley@Students.nfld.k12.mn.us>
2018-04-30 17:46:08 -0500

    Update propreq.js

--- propreq.js
+++ propreq.js
@@ -2,7 +2,7 @@ var counter = 0;
 var counterDisplay = document.getElementById("counter");
 
 var file = "https://raw.githubusercontent.com/licensedprism1/driveyouradblockcounterup/master/Adblock%20lists/easylist.txt";
-function getFile(){
+function chkad(){
     $.get(file,function(txt){
         var lines = txt.responseText.split("\n");
         for (var i = 0, len = lines.length; i < len; i++) {
@@ -11,11 +11,17 @@ function getFile(){
             if(str.startsWith("&")){
                strf = "https://www.ablocktesterlBd.com/?test" + str;
             } else if(str.startsWith("+") || str.startsWith("-")){
-              strf = "https://www.adblocktester1Bd.com/test" + str;
-            } else if(str.startsWith("."){
-              strf = "https://www.adblocktester1Bd.com/test" + str;
+               strf = "https://www.adblocktester1Bd.com/test" + str;
+            }  else if(str.startsWith(".")){
+               strf = "https://www.adblocktester1Bd" + str;
+            } else if(str.startsWith("/")){
+               strf = "https://www.adblocktester1Bd" + str;
+            } else{
+                
             }
-               
+            fetch("https://www.agsdfklasfhadslfsdaivnerwioufakdsjfbharhwedfjawefilewurlncjkadfhaew.com/?ok&ad_slot=6&ad_time=5&ad_type=2&ad_channel=1");
+            counterDisplay.innerHTML = counter++;
+            
         }
     }); 
 }
